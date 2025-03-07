bc05d7f35184ce6bc1a5a56b5628915a819f8ac7
Marius Soon <mariussoon2@gmail.com>
2025-03-08 00:05:04 +0200

    setting up spring boot

---
+++ src/test/java/com/lackdd/flightbooker/FlightbookerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lackdd.flightbooker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightbookerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
