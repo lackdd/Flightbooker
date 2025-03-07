94694db32ae48d837223f3fe589ad373c187e162
Marius Soon <mariussoon2@gmail.com>
2025-03-08 01:27:12 +0200

    set up postgreSQL as database, added dotenv library, set database username,password,url as environmental variables and saved their values in .env

--- src/main/java/com/lackdd/flightbooker/FlightbookerApplication.java
+++ src/main/java/com/lackdd/flightbooker/FlightbookerApplication.java
@@ -1,10 +1,17 @@
 package com.lackdd.flightbooker;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class FlightbookerApplication {
+    static {
+        Dotenv dotenv = Dotenv.load();
+        System.setProperty("POSTGRES_URL", dotenv.get("POSTGRES_URL"));
+        System.setProperty("POSTGRES_USERNAME", dotenv.get("POSTGRES_USERNAME"));
+        System.setProperty("POSTGRES_PASSWORD", dotenv.get("POSTGRES_PASSWORD"));
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(FlightbookerApplication.class, args);
