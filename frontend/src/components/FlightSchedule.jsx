7ae3a099022cf8263e8ef524e1a5d6a47d267394
Marius Soon <mariussoon2@gmail.com>
2025-03-10 00:54:34 +0200

    installed, learned react-table, changed plain text into react-table format

--- frontend/src/components/FlightSchedule.jsx
+++ frontend/src/components/FlightSchedule.jsx
@@ -1,6 +1,78 @@
 import {useEffect, useState} from "react";
 import axios from "axios";
+import {createColumnHelper, flexRender, getCoreRowModel, useReactTable} from '@tanstack/react-table'
 
+
+const columnHelper = createColumnHelper();
+
+const columns = [
+    columnHelper.accessor('flightNumber', {
+        cell: info => info.getValue(),
+        header: () => 'Flight Number',
+        footer: info => info.column.id,
+    }),
+    columnHelper.accessor(row => row.startingLocation, {
+        id: 'startingLocation',
+        cell: info => info.getValue(),
+        header: () => 'Starting Location',
+        footer: info => info.column.id,
+    }),
+/*    columnHelper.accessor('startingLocationCity', {
+        header: () => 'Starting Location City',
+        cell: info => info.getValue(),
+        footer: info => info.column.id,
+    }),
+    columnHelper.accessor('startingLocationCountry', {
+        header: () => 'Starting Location Country',
+        cell: info => info.getValue(),
+        footer: info => info.column.id,
+    }),
+    columnHelper.accessor('startingLocationIATA', {
+        header: () => 'Starting Location IATA',
+        cell: info => info.getValue(),
+        footer: info => info.column.id,
+    }),*/
+    columnHelper.accessor('destination', {
+        header: () => 'Destination',
+        cell: info => info.getValue(),
+        footer: info => info.column.id,
+    }),
+/*    columnHelper.accessor('destinationCity', {
+        header: () => 'Destination City',
+        cell: info => info.getValue(),
+        footer: info => info.column.id,
+    }),
+    columnHelper.accessor('destinationCountry', {
+        header: () => 'Destination Country',
+        cell: info => info.getValue(),
+        footer: info => info.column.id,
+    }),
+    columnHelper.accessor('destinationIATA', {
+        header: () => 'Destination IATA',
+        cell: info => info.getValue(),
+        footer: info => info.column.id,
+    }),*/
+    columnHelper.accessor('price', {
+        header: () => 'Price',
+        cell: info => info.getValue(),
+        footer: info => info.column.id,
+    }),
+    columnHelper.accessor('startingDateTime', {
+        header: () => 'Starting Date and Time',
+        cell: info => info.getValue(),
+        footer: info => info.column.id,
+    }),
+    columnHelper.accessor('arrivalDateTime', {
+        header: () => 'Arrival Date and Time',
+        cell: info => info.getValue(),
+        footer: info => info.column.id,
+    }),
+    columnHelper.accessor('duration', {
+        header: () => 'Duration',
+        cell: info => info.getValue(),
+        footer: info => info.column.id,
+    }),
+];
 
 function FlightSchedule() {
     const [flights, setFlights] = useState([]);
@@ -18,27 +90,60 @@ function FlightSchedule() {
         fetchFlights();
     }, [])
 
-
+    const table = useReactTable({
+        data: flights,
+        columns,
+        getCoreRowModel: getCoreRowModel(),
+    })
 
     return (
         <>
             <h1>Teretulemast Flightbooki! Palun plaanige oma lennuplaan.</h1>
-            {
-                flights.map(f => <p key={f.id}>{[
-                        f.flightNumber,
-                        f.startingLocation,
-                        f.startingLocationCity,
-                        f.startingLocationCountry,
-                        f.startingLocationIATA,
-                        f.destination,
-                        f.destinationCity,
-                        f.destinationCountry,
-                        f.destinationIATA,
-                        f.startingDateTime,
-                        f.arrivalDateTime,
-                        f.price
-                    ].join(" | ")}</p>
-                )}
+            <table>
+                <thead>
+                {table.getHeaderGroups().map(headerGroup => (
+                    <tr key={headerGroup.id}>
+                        {headerGroup.headers.map(header => (
+                            <th key={header.id}>
+                                {header.isPlaceholder
+                                ? null
+                                    : flexRender(
+                                        header.column.columnDef.header,
+                                        header.getContext()
+                                    )}
+                            </th>
+                        ))}
+                    </tr>
+                ))}
+                </thead>
+                <tbody>
+                {table.getRowModel().rows.map(row => (
+                    <tr key={row.id}>
+                        {row.getVisibleCells().map(cell => (
+                            <td key={cell.id}>
+                                {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                            </td>
+                        ))}
+                    </tr>
+                ))}
+                </tbody>
+                <tfoot>
+                {table.getFooterGroups().map(footerGroup => (
+                    <tr key={footerGroup.id}>
+                        {footerGroup.headers.map(header => (
+                            <th key={header.id}>
+                                {header.isPlaceholder
+                                ? null
+                                : flexRender(
+                                    header.column.columnDef.footer,
+                                        header.getContext()
+                                    )}
+                            </th>
+                        ))}
+                    </tr>
+                ))}
+                </tfoot>
+            </table>
         </>
     );
 }
