efb0fbcf1b60b4131568af3e508935811826923d
Marius Soon <mariussoon2@gmail.com>
2025-03-13 01:08:22 +0200

    tried doing a column creation helper function first but had some problems, just made 6 columns and assigned each one 16 seats through iteration might change later

--- frontend/src/components/flight-seats/FlightSeats.jsx
+++ frontend/src/components/flight-seats/FlightSeats.jsx
@@ -7,13 +7,53 @@ function FlightSeats() {
     const location = useLocation();
     const flight = location.state?.flight;
     const [seats, setSeats] = useState([]);
+    const [columns, setColumns] = useState([]);
+    const [columnA, setColumnA] = useState([]);
+    const [columnB, setColumnB] = useState([]);
+    const [columnC, setColumnC] = useState([]);
+    const [columnD, setColumnD] = useState([]);
+    const [columnE, setColumnE] = useState([]);
+    const [columnF, setColumnF] = useState([]);
 
+    // i could try creating 6 arrays of columns each containing 16 seats and then saving them to seats
     useEffect(() => {
 
         const getFlightSeats = async () => {
             try {
                 const response = await axios.get(`http://localhost:8080/api/seats/${flight.id}`)
-                setSeats(response.data);
+                const listOfColumns = [];
+                const ccolumnA = [];
+                const ccolumnB = [];
+                const ccolumnC = [];
+                const ccolumnD = [];
+                const ccolumnE = [];
+                const ccolumnF = [];
+                for (let i = 0; i < 96; i++) {
+                    if(i < 16) {
+                        ccolumnA.push(response.data[i])
+                    }
+                    if(i > 15 && i < 32) {
+                        ccolumnB.push(response.data[i])
+                    }
+                    if(i > 31 && i < 48) {
+                        ccolumnC.push(response.data[i])
+                    }
+                    if(i > 47 && i < 64) {
+                        ccolumnD.push(response.data[i])
+                    }
+                    if(i > 63 && i < 80) {
+                        ccolumnE.push(response.data[i])
+                    }
+                    if(i > 79 && i < 96) {
+                        ccolumnF.push(response.data[i])
+                    }
+                }
+                setColumnA(ccolumnA);
+                setColumnB(ccolumnB);
+                setColumnC(ccolumnC);
+                setColumnD(ccolumnD);
+                setColumnE(ccolumnE);
+                setColumnF(ccolumnF);
             } catch (error) {
                 console.log(error.message);
             }
@@ -25,16 +65,55 @@ function FlightSeats() {
     return (
         <div className="flight-seats-container">
             <h1>Select Seats for Flight {flight.flightNumber}</h1>
-            <p>From: {flight.startingLocation} → To: {flight.destination}</p>
+            {/*<p>From: {flight.startingLocation} → To: {flight.destination}</p>
             <p>Departure: {flight.startingDateTime}</p>
             <p>Arrival: {flight.arrivalDateTime}</p>
-            <p>Price: {flight.price}€</p>
+            <p>Price: {flight.price}€</p>*/}
             <div>
-                {seats.map(seat => <p key={seat.id}>{seat.seatNumber}</p>)}
-
+                <p>
+                    {columnA.map(seat => (
+                        <button key={seat.id} className="seat" style={{marginRight: "5px"}}>
+                            {seat.seatNumber}
+                        </button>
+                    ))}
+                </p>
+                <p>
+                    {columnB.map(seat => (
+                        <button key={seat.id} className="seat" style={{marginRight: "5px"}}>
+                            {seat.seatNumber}
+                        </button>
+                    ))}
+                </p>
+                <p>
+                    {columnC.map(seat => (
+                        <button key={seat.id} className="seat" style={{marginRight: "5px"}}>
+                            {seat.seatNumber}
+                        </button>
+                    ))}
+                </p>
+                <p>
+                    {columnD.map(seat => (
+                        <button key={seat.id} className="seat" style={{marginRight: "5px"}}>
+                            {seat.seatNumber}
+                        </button>
+                    ))}
+                </p>
+                <p>
+                    {columnE.map(seat => (
+                        <button key={seat.id} className="seat" style={{marginRight: "5px"}}>
+                            {seat.seatNumber}
+                        </button>
+                    ))}
+                </p>
+                <p>
+                    {columnF.map(seat => (
+                        <button key={seat.id} className="seat" style={{marginRight: "5px"}}>
+                            {seat.seatNumber}
+                        </button>
+                    ))}
+                </p>
             </div>
             {/* TODO: Add seat selection logic here */}
-            <button onClick={() => alert("Seat Selected!")}>Select Seat</button>
         </div>
     )
 }
